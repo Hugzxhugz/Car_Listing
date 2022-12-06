e5f2ae119b5d03175a2345108ba61d1591e10cd7
Hugzxhugz <jameskenthagman@gmail.com>
2022-12-06 14:21:45 +0100

    added button

--- app/src/main/java/se/malmo/carlisting/MainActivity.java
+++ app/src/main/java/se/malmo/carlisting/MainActivity.java
@@ -3,6 +3,7 @@ package se.malmo.carlisting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void onAddButtonClick(View view){
+
+
+    }
+
 }
