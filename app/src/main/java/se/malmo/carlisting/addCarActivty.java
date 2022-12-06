f5930c1b507545aad45b7210e83695b3ff002d64
Hugzxhugz <jameskenthagman@gmail.com>
2022-12-06 14:37:54 +0100

    added addCarActivty

---
+++ app/src/main/java/se/malmo/carlisting/addCarActivty.java
@@ -0,0 +1,14 @@
+package se.malmo.carlisting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addCarActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_car_activty);
+    }
+}
